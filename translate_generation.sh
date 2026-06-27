02baaf9353dac5a16358cdccdb5a314ecbd033d0
MarcusPy827 <markus_verify@126.com>
2026-06-28 00:13:31 +0800

    fix: 修复lrelease问题
    (cherry picked from commit 6a129b792f6eb8cf9f514bc7a3ce6d7a5f7b1fa2)

--- translate_generation.sh
+++ translate_generation.sh
@@ -2,10 +2,17 @@
 # this file is used to auto-generate .qm file from .ts file.
 # author: shibowen at linuxdeepin.com
 
-ts_list=(`ls */translations/*.ts`)
+# Qt6: /usr/lib/qt6/bin/lrelease, Qt5: lrelease (via qtchooser)
+if [ -x /usr/lib/qt6/bin/lrelease ]; then
+    LRELEASE=/usr/lib/qt6/bin/lrelease
+else
+    LRELEASE=lrelease
+fi
+
+ts_list=(`ls */translations/*.ts 2>/dev/null`)
 
 for ts in "${ts_list[@]}"
 do
     printf "\nprocess ${ts}\n"
-    lrelease "${ts}"
+    $LRELEASE "${ts}"
 done
